6a3704cb084ca29d4e2647679aca67f8f1064f50
Zhenkar Gowda K P <151987799+Zhenkar@users.noreply.github.com>
2025-11-27 22:18:25 +0530

    ....

--- myapp-frontend/src/components/Navbar.jsx
+++ myapp-frontend/src/components/Navbar.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Navbar = ({ user, onLogout }) => {
+
+  const navigate = useNavigate();
+
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");      // update state to hide button
+    navigate("/login");  // redirect to login
+  };
+
   return (
     <nav className="bg-blue-500 p-4 text-white flex justify-between items-center shadow-md">
       <h1 className="text-xl font-bold">Notes App</h1>
@@ -8,7 +20,7 @@ const Navbar = ({ user, onLogout }) => {
         <div className="flex items-center gap-4">
           <span>Hello, {user.username}</span>
           <button
-            onClick={onLogout}
+            onClick={handleLogout}
             className="bg-white text-blue-500 px-3 py-1 rounded hover:bg-gray-100"
           >
             Logout
